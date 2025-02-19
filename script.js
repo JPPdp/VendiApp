10e4a3d7e76c597d0cb9c8f073e01974e9e80b57
LibronJims <anfe.jacinto.up@phinmaed.com>
2025-02-20 03:15:58 +0800

    Styling changes for index and registration form

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const passwordInput = document.getElementById('CLIENT_PASSWORD');
-    const confirmPasswordInput = document.getElementById('CLIENT_CONFRIMPASSWORD');
+    const CLIENTbtn = document.getElementById('btnCLIENT');
+    const VENDORbtn = document.getElementById('btnVENDOR');
+
+    const CLIENTform = document.getElementById('CLIENT_FORM');
+    const VENDORform = document.getElementById('VENDOR_FORM');
+
+    CLIENTform.classList.add('active');
+    CLIENTbtn.classList.add('active');
+
+    CLIENTbtn.addEventListener('click', function () {
+        CLIENTform.classList.add('active');
+        VENDORform.classList.remove('active');
+
+        CLIENTbtn.classList.add('active');
+        VENDORbtn.classList.remove('active');
+    });
+
+    VENDORbtn.addEventListener('click', function () {
+        VENDORform.classList.add('active');
+        CLIENTform.classList.remove('active');
+
+        VENDORbtn.classList.add('active');
+        CLIENTbtn.classList.remove('active');
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const passwordInput = document.getElementById('PASSWORD');
+    const confirmPasswordInput = document.getElementById('CONFIRM_PASSWORD');
     const passwordToggle = document.getElementById('password-toggle');
-    const confirmPasswordToggle = document.getElementById('confirm-password-toggle');
+    const confirmPasswordToggle = document.getElementById('confirm-password-toggle'); 
 
     passwordToggle.addEventListener('click', function () {
         togglePasswordVisibility(passwordInput, passwordToggle);
